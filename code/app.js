f33e8f333c8bcfff0aafd9a5425da8daec227e47
Esteve <ets1995@hotmail.com>
2017-03-30 09:14:59 +0200

    added cross-origin

--- code/app.js
+++ code/app.js
@@ -81,7 +81,11 @@ var app = express();
 
 //making files in public served at /
 app.use(express.static('public'))
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
