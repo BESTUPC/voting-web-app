2a0ed9173de2096de148b1868deca4d9c6b992bc
Iñigo Moreno <ignigomoreno@gmail.com>
2017-05-12 16:47:36 +0200

    Trying to solve bug in mobile.

--- code/public/js/navbar.js
+++ code/public/js/navbar.js
@@ -59,7 +59,7 @@ function initNavBar(profile){
         if(isAdmin){
         	$('#sidebar').show();
         	$('#navbar-toggle').show();
-        	$('#page-wrapper').css('margin-left','250px');
+        	$('#page-wrapper-style').text('@media (min-width: 768px) { #page-wrapper { margin: 0 0 0 250px; }  }');
     	}
     };
     xhrNav.send('userId='+profile.id);
