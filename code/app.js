d36eff334fda5fe32b8ebbeae4d00d41fcb005a5
adriaq <adria.quesada.alacid@est.fib.upc.edu>
2018-01-26 13:19:14 +0100

    shan borrat dos funcions que no es feien servir

--- code/app.js
+++ code/app.js
@@ -12,20 +12,7 @@ var client = new auth.OAuth2(CLIENT_ID, CLIENT_SECRET, "http://localhost:3000/")
 var bodyParser = require("body-parser");
 var crypto = require('crypto'), algorithm = 'aes-256-ctr', password = CLIENT_SECRET;
 
-function codificate(userId, option, pollId) {
-  var cipher = crypto.createCipher(algorithm,password);
-  var encrypted = cipher.update(userId + option + pollId, 'utf8', 'hex');
-  return encrypted;
-}
 
-function decodificate(userId, options, pollId, encrypted) {
-  var cipher = crypto.createCipher(algorithm,password);
-  for (optionkey in options){
-    var auxEncrypted = cipher.update(userId + options[optionkey] + pollId, 'utf8', 'hex');
-    if (auxEncrypted == encrypted) return options[optionkey];
-  }
-  return null;
-}
 //Connection to mongodb
 // Connection URL
 //var url = 'mongodb://bestbarcelona.org:27017/votacions';
@@ -46,41 +33,6 @@ MongoClient.connect(url, function(err, db) {
   }
 });
 
-/*
-//Create the stub, this should be deleted on the final version
-MongoClient.connect(url, function(err, db) {
-  var user = {};
-  user['userId'] = "102462623932080066899";
-  user['membership'] = ["admin", "full", "all"];
-  user['name'] = "Petter Varlac";
-  user['email'] = "petter@best.com";
-  db.collection('users').insertMany([user], function(err, result) {});
-  var votacio = {};
-  votacio['_id'] = new ObjectID("3456789");
-  votacio['pollName'] = "Bestie de la biSetmana";
-  votacio['pollOptions'] = ["Iñigo", "Quesito", "Bernat", "Laia"];
-  votacio['targetGroup'] = "all";
-  votacio['isPrivate'] = false;
-  votacio['pollDeadline'] = "3234672825";
-  votacio['state'] = "open";
-  votacio['description'] = "descripció ... why?";
-  db.collection('votacions').insertMany([votacio], function(err, result){});
-  var vote = {};
-  vote['pollId'] = "3456789";
-  vote['userId'] = "102462623932080066899";
-  vote['pollOption'] = "Bernat";
-  db.collection('votes').insertMany([vote], function(err, result){});
-  var withdrawal = {};
-  withdrawal['pollId'] = "3456789";
-  withdrawal['userId'] = "102462623932080066899";
-  db.collection('askWithdrawal').insertMany([withdrawal], function(err, result){});
-  var privat = {};
-  privat['pollId'] = "3456789";
-  privat['userId'] = "102462623932080066899";
-  db.collection('askPrivate').insertMany([privat], function(err, result){});
-  db.close();
-});*/
-
 //Creating the webserver
 var app = express();
 
@@ -95,7 +47,7 @@ app.use(function(req, res, next) {
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-//What file to return when acces to '/'
+//What file to return when acces to '/' Pendent de borrar, creiem que no fa res
 app.get('/', function (req, res) {
   var options = {
     root: __dirname + '/public/',
