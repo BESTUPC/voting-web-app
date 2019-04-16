b12b51da9002768dc3dcdd0523be651d408f3e39
root <root@DESKTOP-RI6KFHE.localdomain>
2019-04-16 15:47:47 +0200

    Start http if no credentials found

--- code/app.js
+++ code/app.js
@@ -17,16 +17,6 @@ const http = require('http');
 
 const https = require('https');
 
-// Certificate
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/bestbarcelona.org/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/bestbarcelona.org/cert.pem', 'utf8');
-const ca = fs.readFileSync('/etc/letsencrypt/live/bestbarcelona.org/chain.pem', 'utf8');
-
-const credentials = {
-	key: privateKey,
-	cert: certificate,
-	ca: ca
-};
 
 //Connection to mongodb
 // Connection URL
@@ -1468,15 +1458,42 @@ app.post('/removePoll', function (req, res) {
 
 //API calls end here
 
+if (fs.existsSync('/etc/letsencrypt/live/bestbarcelona.org/privkey.pem') &&
+    fs.existsSync('/etc/letsencrypt/live/bestbarcelona.org/cert.pem') &&
+    fs.existsSync('/etc/letsencrypt/live/bestbarcelona.org/chain.pem')) {
+  
+  // Certificate found
+  const privateKey = fs.readFileSync('/etc/letsencrypt/live/bestbarcelona.org/privkey.pem', 'utf8');
+  const certificate = fs.readFileSync('/etc/letsencrypt/live/bestbarcelona.org/cert.pem', 'utf8');
+  const ca = fs.readFileSync('/etc/letsencrypt/live/bestbarcelona.org/chain.pem', 'utf8');
 
-//Definig the port in which will run our app
-const httpServer = http.createServer(app);
-const httpsServer = https.createServer(credentials, app);
+  const credentials = {
+    key: privateKey,
+    cert: certificate,
+    ca: ca
+  };
 
-httpServer.listen(3003, () => {
-	console.log('HTTP Server running on port 3003');
-});
+  //Definig the port in which will run our app
+  const httpServer = http.createServer(app);
+  const httpsServer = https.createServer(credentials, app);
 
-httpsServer.listen(3000, () => {
-	console.log('HTTPS Server running on port 3000');
-});
+  httpServer.listen(3003, () => {
+    console.log('HTTP Server running on port 3003');
+  });
+
+  httpsServer.listen(3000, () => {
+    console.log('HTTPS Server running on port 3000');
+  });
+}
+else{
+ // Certificate not found
+ console.log('HTTPS certificate not found, starting http')
+  //Definig the port in which will run our app
+  const httpServer = http.createServer(app);
+
+  httpServer.listen(3000, () => {
+    console.log('HTTP Server running on port 3000');
+  });
+
+}
+
