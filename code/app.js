4d990b2f26de2232fa30274dd03aa556c4f02bc0
Bernat Felip <bernat.felip@goin.app>
2019-12-02 19:51:31 +0100

    console logs

--- code/app.js
+++ code/app.js
@@ -10,8 +10,6 @@ var CLIENT_ID = obj['id'];
 var CLIENT_SECRET = obj['secret'];
 var client = new auth.OAuth2(CLIENT_ID, CLIENT_SECRET, "https://localhost:3000/");
 var bodyParser = require("body-parser");
-var crypto = require('crypto'), algorithm = 'aes-256-ctr', password = CLIENT_SECRET;
-//var credentials = {key: privateKey, cert: certificate};
 
 const http = require('http');
 const https = require('https');
